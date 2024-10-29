d9f532c1f9cfecfbe7124976d606ad3f12886cd4
rasel3980 <rkrasel3980@gmail.com>
2024-10-29 11:14:19 +0600

    added a projects live link in readme.md file

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,9 +1,10 @@
 import assets from '/assets/bg-shadow.png'
+import bannerImg from '../../../assets/banner-main.png'
 const Banner = ({handleCoinCount}) => {
     return (
         <div style={{ backgroundImage: `url(${assets})` }} className="bg-cover bg-center h-screen bg-black w-11/12 mx-auto text-white mt-8 rounded-xl">
             <div className='flex justify-center items-center pt-14'>
-            <img src="/assets/banner-main.png" alt="" />
+            <img src={bannerImg} alt="" />
             </div>
             <h1 className='text-4xl font-bold flex justify-center mt-7'>Assemble Your Ultimate Dream 11 Cricket Team</h1>
             <p className='text-2xl flex justify-center mt-7 text-gray-300'>Beyond Boundaries Beyond Limits</p>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,11 @@
+import logoImg from '../../../assets/logo.png';
+import coinLogo from '../../../assets/download.png';
 
 const Header = ({price}) => {
     return (
         <div className="sticky top-0 z-10 bg-white flex justify-between items-center w-11/12 mt-12 mx-auto shadow-lg">
             <div>
-                <img src="/assets/logo.png" alt="Logo" />
+                <img src={logoImg} alt="Logo" />
             </div>
             <div className="flex gap-12 justify-center items-center text-gray-500 font-bold">
                 <a href="/">Home</a>
@@ -12,7 +14,7 @@ const Header = ({price}) => {
                 <a href="/schedules">Schedules</a>
                 <div>
                     <button className="border border-solid rounded-xl px-5 flex items-center gap-2">
-                        {price} Coin <img className="w-8" src="/assets/download.png" alt=" Coin icon" />
+                        {price} Coin <img className="w-8" src={coinLogo} alt=" Coin icon" />
                     </button>
                 </div>
             </div>
